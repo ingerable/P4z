67c31a481445591f0b87ef4431866cef533f134f
Julien Gossa <julien.gossa@unistra.fr>
2017-01-26 12:42:45 +0100

    parallelisation des executions

--- 101/perf.sh
+++ 101/perf.sh
@@ -1,31 +1,38 @@
 #! /bin/bash
 
-NB_TEST=30
+NB_TEST=50
 
 FUNS="generer_tableau afficher_tableau rechercher"
 
 EXES="rechercheV0 recherche"
 
-echo -e "test\ttaille\texe\texectime\tmemory\tgenerer_tableau\tafficher_tableau\trechercher"
+echo -e "test\ttaille\texe\texectime\tmemory\tgenerer_tableau\tafficher_tableau\trechercher" > perf.dat
+
 
 for test in `seq $NB_TEST`
 do
 	param="${RANDOM}000"
 
+	PIDS=""
+
 	for exe in $EXES
 	do
 		(
-			echo -en "$test\t$param\t$exe\t"
-			echo -en `(/usr/bin/time -f "%U\t%M" ./$exe $param $test > /dev/null) 2>&1`
+			RES="$test\t$param\t$exe\t"
+			RES="$RES`(/usr/bin/time -f "%U\t%M" ./$exe $param $test > /dev/null) 2>&1`"
 
-			gprof $exe > /tmp/gprof.out
+			gprof $exe > /tmp/gprof.$exe.out
 			for fun in $FUNS
 			do
-				echo -en "\t`cat /tmp/gprof.out | grep $fun | awk '{print $6}' | head -1`"
+				RES="$RES\t`cat /tmp/gprof.$exe.out | grep $fun | awk '{print $6}' | head -1`"
 			done
 
-			echo 
-		) &
+			echo -e $RES
+
+		)  &
+		PIDS="$PIDS $!"
 	done
+
+	wait $PIDS
 
 done
