26580bba713f16fb03077ca3cef33be5e4b6fee5
Julien Gossa <julien.gossa@unistra.fr>
2017-01-26 12:25:35 +0100

    deux executables testes

--- 101/perf.R
+++ 101/perf.R
@@ -5,3 +5,8 @@ ggplot(data=perf, aes(x=taille,y=exectime)) + geom_point() + geom_smooth()
 ggplot(data=perf, aes(x=taille,y=rechercher)) + geom_point() + geom_smooth() + geom_point(aes(y=generer_tableau),color="red") + geom_smooth(aes(y=generer_tableau),color="red")
 
 ggplot(data=perf, aes(x=taille,y=rechercher)) + geom_point() + geom_smooth() + geom_point(data=perfv1,color="red") + geom_smooth(data=perfv1,color="red")
+
+> perf <- read.table("perf.dat", header=TRUE)
+> perfV1 <- perf[perf$exe == 'recherche',]
+> perfV0 <- perf[perf$exe == 'rechercheV0',]
+> ggplot(data=perfV0, aes(x=taille,y=rechercher)) + geom_point() + geom_smooth() + geom_point(data=perfV1,color="red") + geom_smooth(data=perfV1,color="red")
--- 101/perf.sh
+++ 101/perf.sh
@@ -1,24 +1,31 @@
 #! /bin/bash
 
-NB_TEST=10
+NB_TEST=30
 
 FUNS="generer_tableau afficher_tableau rechercher"
 
-echo -e "test\ttaille\texectime\tmemory\tgenerer_tableau\tafficher_tableau\trechercher"
+EXES="rechercheV0 recherche"
+
+echo -e "test\ttaille\texe\texectime\tmemory\tgenerer_tableau\tafficher_tableau\trechercher"
 
 for test in `seq $NB_TEST`
 do
 	param="${RANDOM}000"
 
-	echo -en "$test\t$param\t"
-	echo -en `(/usr/bin/time -f "%U\t%M" ./recherche $param > /dev/null) 2>&1`
-	
-	gprof recherche > /tmp/gprof.out
-	for fun in $FUNS
+	for exe in $EXES
 	do
-		echo -en "\t`cat /tmp/gprof.out | grep $fun | awk '{print $6}' | head -1`"
-	done
+		(
+			echo -en "$test\t$param\t$exe\t"
+			echo -en `(/usr/bin/time -f "%U\t%M" ./$exe $param $test > /dev/null) 2>&1`
 
-	echo 
+			gprof $exe > /tmp/gprof.out
+			for fun in $FUNS
+			do
+				echo -en "\t`cat /tmp/gprof.out | grep $fun | awk '{print $6}' | head -1`"
+			done
+
+			echo 
+		) &
+	done
 
 done
